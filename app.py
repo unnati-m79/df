b39bc5637df6538a1ad621808ff1855caf1a6062
unnati-m79 <m.unnati7905@gmail.com>
2025-06-19 12:25:44 +0530

    app.py

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ st.markdown("Enter the details below to predict the price of a house:")
 
 # Input fields
 area = st.number_input("Area (in square feet)", min_value=100, max_value=10000, value=1500)
-bedrooms = st.number_input("Number of Bedrooms", min_value=1, max_value=10, value=3)
-age = st.number_input("Age of the House (in years)", min_value=0, max_value=100, value=10)
+
 
 # Prediction
 if st.button("Predict Price"):
